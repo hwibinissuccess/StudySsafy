4ffce6713143521b019ffb041d0df7e591c76453
hwibinissuccess <chlgnlqls3@nate.com>
2023-04-24 15:36:09 +0900

    SSAFY study

---
+++ Spring_01_DI_5_JavaCofig/src/com/ssafy/di/Laptop.java
@@ -0,0 +1,11 @@
+package com.ssafy.di;
+
+import org.springframework.stereotype.Component;
+
+public class Laptop implements Computer{
+	// 필드명 작성
+	// CPU, 그래픽카드, 키보드 등등 사양
+	public String getInfo() {
+		return "노트북";
+	}
+}
---
+++ Spring_01_DI_5_JavaCofig/src/com/ssafy/di2/Programmer.java
@@ -0,0 +1,26 @@
+package com.ssafy.di2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Programmer {
+	private Computer computer;
+
+	public Programmer() {
+	}
+
+	public Programmer(Computer computer) {
+		this.computer = computer;
+	}
+
+	@Autowired
+	public void setComputer(@Qualifier("laptop") Computer computer) {
+		this.computer = computer;
+	}
+
+	public void coding() {
+		System.out.println(computer.getInfo() + "으로 개발을 합니다.");
+	}
+}
