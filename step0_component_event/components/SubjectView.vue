9bdbe2ffef63f66ea30abe4930c35658b613de04
hwibinissuccess <chlgnlqls3@nate.com>
2023-05-11 14:37:56 +0900

    one step

---
+++ step0_component_event/components/SubjectView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="addCount">{{ title }} - {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubjectView",
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.count += 1;
+      this.$emit('add-to-count')
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ step2_getters/components/ResultView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>전체 {{total}} 번 클릭됨</h2>
+    <h3>{{countMsg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ResultView",
+    computed: {
+      total() {
+        return this.$store.state.total
+      },
+      countMsg: function (){
+        return this.$store.getters.countMsg
+      }
+      // countMsg(){
+      //   return this.$store.getters.countMsg
+      // }
+    }
+}
+</script>
+
+<style>
+
+</style>
